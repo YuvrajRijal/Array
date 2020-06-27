c1ffa3569855cbc89709821eabf6fd3cf7985ac6
Yuvi <rijal.yuvraj01@gmail.com>
2020-06-27 15:07:36 +0530

    first commit

---
+++ JavaDemo/src/Demo.java
@@ -0,0 +1,23 @@
+
+public class Demo {
+//
+////public static void main(String[] args) {
+////	
+////}
+//public static void main(String... args) {
+//	
+////}
+////public static void main(String args) {
+////	
+////}
+////	
+//	public static void main(String[] args) {
+//		System.out.println("Hello!");
+//	}
+
+        public static void main(String[] args) {
+		System.out.println("Hello");
+	
+	
+}
+}
